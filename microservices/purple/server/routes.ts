792a319a528f9ad9cbeaed4ab79453f5c0e6c667
Shashank Shekhar <shashekh@iu.edu>
2019-01-22 14:10:22 -0500

    travis test

--- microservices/purple/server/routes.ts
+++ microservices/purple/server/routes.ts
@@ -3,7 +3,7 @@ import { Request, Response, Application } from 'express';
 export class Routes {
 
     routes(app: Application): void {
-        app.route('/test').get((req: Request, res: Response) => {
+        app.route('/test1').get((req: Request, res: Response) => {
             res.sendStatus(200);
         });
     }
