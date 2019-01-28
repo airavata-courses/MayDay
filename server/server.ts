ea4cc74185468d65c52c9c370d9581bbda089545
Shashank Shekhar <shashekh@iu.edu>
2019-01-28 13:38:31 -0500

    reverted to env port

--- server/server.ts
+++ server/server.ts
@@ -3,5 +3,5 @@ import app from './app';
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT,() => {
-    console.log('Express server listening on port ' + 3000);
+    console.log('Express server listening on port ' + PORT);
 });
