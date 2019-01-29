d339c9747af424c5afc31585197bcde547f79065
Shashank Shekhar <shashekh@iu.edu>
2019-01-29 11:31:23 -0500

    fixed port error and travis bg server

--- server/__init__.py
+++ server/__init__.py
@@ -2,5 +2,5 @@ from app import App
 from waitress import serve
 import os
 
-PORT = os.environ.get['PORT',8000]
+PORT = os.environ.get('PORT',8000)
 serve(App().get_api(), host='localhost', port=PORT)
