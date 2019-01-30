e4174136a566111281a4f82663c9d58f9a601cbf
rojraman <rojraman@iu.edu>
2019-01-30 02:39:45 -0500

    Initial component for home login and navigation panel

---
+++ src/app/navigation-panel/navigation-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation-panel',
+  templateUrl: './navigation-panel.component.html',
+  styleUrls: ['./navigation-panel.component.css']
+})
+export class NavigationPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
