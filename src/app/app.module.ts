090cf8792b813d02d796fb0209587de7b1e7d766
rojraman <rojraman@iu.edu>
2019-01-30 04:24:18 -0500

    deleted navigation panel component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
-import { NavigationPanelComponent } from './navigation-panel/navigation-panel.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent,
-    NavigationPanelComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
