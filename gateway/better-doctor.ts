baabd206caf38ec99e24a22876afdd292dae4e51
Shashank Shekhar <shashekh@iu.edu>
2019-01-31 21:54:32 -0500

    written basic better doctor rest gateway

---
+++ gateway/better-doctor.ts
@@ -0,0 +1,42 @@
+import * as reqProm from 'request-promise';
+import * as _ from 'lodash';
+import * as fs from 'fs';
+
+export class Search {
+
+    requestOptions: any
+    httpsRequestPromise: any;
+    requestParams: any
+    constructor(type: string, requestParams: any, uid: string = 'none') {
+        this.requestParams = requestParams;
+        this.requestOptions = {
+            uri: this.getRestURL(type, uid) + '?' + this.createRequestUri() + '&user_key=9b42a26b03720362acc1afe40bd3372a',
+            method: 'GET',
+            json: true
+        }
+    }
+
+    getRestURL(type: string, uid: string): string {
+        const content = fs.readFileSync('server.json', 'utf-8');
+        const restConfig = JSON.parse(content);
+        let url = restConfig[type];
+        if (url) {
+            url = url.replace('$$uid', uid);
+        }
+        console.log(url);
+        return url;
+    }
+
+    requestApi(): Promise<any> {
+        return reqProm(this.requestOptions);
+    }
+
+    createRequestUri(): string {
+        let queryString: string[] = [];
+       _.forEach(this.requestParams, (value, key) => {
+            queryString.push(key+'='+value);
+       });
+       return queryString.join('&')
+    }
+
+}
--- server/routes.ts
+++ server/routes.ts
@@ -1,10 +1,38 @@
-import { Request, Response, Application } from 'express';
+import { Request, Response, Application, response } from 'express';
+import { Search } from '../gateway/better-doctor';
+import * as _ from 'lodash';
 
 export class Routes {
 
+    alllowedPaths: string[];
+    constructor() {
+        this.alllowedPaths = ['alldoctors', 'specificdoctor', 'allpractices', 'specificpractice', 'doctorsinpractice', ]
+    }
     routes(app: Application): void {
+
         app.route('/test').get((req: Request, res: Response) => {
-            res.sendStatus(200);
+            res.status(200);
+        });
+
+        app.route('/*').post(async (req: Request, res: Response) => {
+            const paramsFromRequest: any = req.body;
+            const path = req.path.replace(/\//g,'');
+            console.log(path);
+            if (this.alllowedPaths.indexOf(path) == -1) {
+                res.status(500).send({'error': '/'+path+' not allowed'});
+                return;
+            }
+            const searchInstance = new Search(path, _.omit(paramsFromRequest, ['uid']), paramsFromRequest['uid']);
+            res.set('Content-Type', 'application/json')
+            let jsonData: any = null;
+            let status = 200;
+            await searchInstance.requestApi().then((data) => {
+                jsonData = data;
+            }).catch((err) => {
+                status = 500;
+                jsonData = {'error': err+''};
+            });  
+            res.status(status).send(jsonData);
         });
     }
 }
