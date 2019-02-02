33d0bcc487a10e3517bc57c95d9cb0e6b809ea6e
Shashank Shekhar <shashekh@iu.edu>
2019-02-02 15:43:11 -0500

    reverted to python command to run the server

--- serve.py
+++
@@ -1,3 +0,0 @@
-from server.app import App
-
-app = App().get_api() 
--- server/__init__.py
+++ server/__init__.py
@@ -1,6 +1,6 @@
 from app import App
-#from waitress import serve
-#import os
+from waitress import serve
+import os
 app = App().get_api()
-#PORT = os.environ.get('PORT',8000)
-#serve(App().get_api(), host='localhost', port=PORT)
+PORT = os.environ.get('PORT',8000)
+serve(App().get_api(), host='localhost', port=PORT)
