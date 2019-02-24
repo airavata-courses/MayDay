29d75219a6be8a38e2472c4cfa274cfb9d831b0a
KalaRajam <shekhar957@gmail.com>
2019-02-24 16:35:30 -0500

    added https support to purple

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,14 @@
 import app from './app';
-
+import * as fs from 'fs';
+import * as https from 'https';
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,() => {
+const options = {
+    cert: fs.readFileSync('cert/ssl_host.crt'),
+    key: fs.readFileSync('cert/ssl_host.key')
+  };
+
+const server = https.createServer(options, app);
+server.listen(PORT,() => {
     console.log('Express server listening on port ' + PORT);
 });
