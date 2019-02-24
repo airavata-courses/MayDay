058a4ab9f575777581c10a55ca2d4600052a8675
KalaRajam <shekhar957@gmail.com>
2019-02-24 16:48:56 -0500

    rever to non ssl

--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,14 @@
 import app from './app';
-import * as fs from 'fs';
-import * as https from 'https';
+//import * as fs from 'fs';
+//import * as https from 'https';
 const PORT = process.env.PORT || 3000;
 
-const options = {
+/*const options = {
     cert: fs.readFileSync('cert/ssl_host.crt'),
     key: fs.readFileSync('cert/ssl_host.key')
   };
 
-const server = https.createServer(options, app);
-server.listen(PORT,() => {
+const server = https.createServer(options, app);*/
+app.listen(PORT,() => {
     console.log('Express server listening on port ' + PORT);
 });
