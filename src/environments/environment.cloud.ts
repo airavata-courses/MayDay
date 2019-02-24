92b04e8cdbd8d7af237ecf811486f7a9ed4e554a
KalaRajam <shekhar957@gmail.com>
2019-02-24 16:39:13 -0500

    updated web service listing to https

--- src/environments/environment.cloud.ts
+++ src/environments/environment.cloud.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     endpoints: {
-      'purple': 'http://149.165.169.31:3000',
+      'purple': 'https://149.165.169.31:3000',
       'blue': 'http://149.165.168.77:7000',
       'red': 'http://149.165.169.159:8080/rest/customers',
       'geocode': 'https://www.mapquestapi.com/geocoding/v1/reverse?key=EvAFDNlMGI6PeGpkR33PAUfF61AvIliz'
