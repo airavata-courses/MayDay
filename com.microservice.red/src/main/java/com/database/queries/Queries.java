234363a46c493c12708050bbe2b781000ab212cb
KalaRajam <shekhar957@gmail.com>
2019-02-25 19:24:52 -0500

    backend junk cleanup for signup #81

--- com.microservice.red/src/main/java/com/database/queries/Queries.java
+++ com.microservice.red/src/main/java/com/database/queries/Queries.java
@@ -6,32 +6,16 @@ public class Queries {
 			.append("CREATE TABLE person (")
 			.append("email VARCHAR(50),")
 			.append("name VARCHAR(50),")
-			.append("password VARCHAR(50),")
-			.append("address VARCHAR(50),")
-			.append("zipcode INT,")
-			.append("latitude NUMBER,")
-			.append("longitude NUMBER,")
-			.append("image_url VARCHAR(50),")
-			.append("logged_in INT,")
-			.append("last_login DATE,")
-			.append("user_id NUMBER")
+			.append("password VARCHAR(50)")
 			.append(")").toString();
 	
 	public static final String PERSONSCHEMACLEANUP = "DROP TABLE IF EXISTS person";
 	
 	public static final String PERSONMOCKDATA = new StringBuilder()
-			.append("INSERT INTO public.person VALUES (")
+			.append("INSERT INTO person VALUES (")
 			.append("'o',")
 			.append("'shashank',")
 			.append("'ss1',")
-			.append("'ssds',")
-			.append("47401,")
-			.append("10,")
-			.append("20,")
-			.append("'http://ola',")
-			.append("0,")
-			.append("'2019-02-01',")
-			.append("101")
 			.append(")")
 			.toString();
 }
