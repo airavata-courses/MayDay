d342355f3c2c0a4a39048fe1a5e45cd52cbf55f6
KalaRajam <shekhar957@gmail.com>
2019-02-27 22:05:17 -0500

    Revert "added portable h2 support in red #91" because of broken build
    This reverts commit 7d0acf06a970c0272280bb1798e4cc559f6a0bfb.

--- com.microservice.red/src/main/java/com/microservice/red/personresource.java
+++ com.microservice.red/src/main/java/com/microservice/red/personresource.java
@@ -1,6 +1,9 @@
 package com.microservice.red;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -8,78 +11,99 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
-
 import org.json.simple.JSONObject;
+
 
 @Path("customers")
 public class personresource {
 
 	personrepository repo = new personrepository();
-
+	
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public List<person> getpersons() {
+	@Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+	public List<person> getpersons()
+	{
+		System.out.println("getpersons Function called!");
+		
 		return repo.getpersons();
 	}
 
 	@GET
 	@Path("customer/{email}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public person getperson(@QueryParam("email") String email) {
+	@Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+	public person getperson(@QueryParam("email") String email)
+	{
+		System.out.println(repo.getperson(email));
 		return repo.getperson(email);
 	}
-
+	
 	@POST
 	@Path("customer")
-	public void createperson(person a1) {
+	public person createperson(person a1)
+	{
+		System.out.println(a1);
 		repo.create(a1);
-
+		
+		return a1;
 	}
-
+	
 	@POST
 	@Path("askLogin")
 	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response askLogin(person p) {
-		person pdb;
-		pdb = repo.getperson(p.getEmail());
-		if (pdb.getEmail() == null) {
-			JSONObject obj = new JSONObject();
-			obj.put("login", "failed");
-			return Response.status(Status.OK).entity(obj.toJSONString()).build();
-		} else if (p.getPassword().equals(pdb.getPassword())) {
-			JSONObject obj = new JSONObject();
-			obj.put("login", "success");
-			obj.put("last_login", pdb.getLast_login().toString());
-			obj.put("image_url", pdb.getImage_url());
-			return Response.status(Status.OK).entity(obj.toJSONString()).build();
-		}
-		JSONObject obj = new JSONObject();
-		obj.put("login", "failed");
-		return Response.status(Status.OK).entity(obj.toJSONString()).build();
+	    person pdb;
+	    pdb = repo.getperson(p.getEmail());
+	    if (pdb.getEmail() == null) {
+	    	JSONObject obj = new JSONObject();
+	    	obj.put("login", "failed");
+	    	return Response.status(Status.OK).entity(obj.toJSONString()).build();
+	    }
+	    else if (p.getPassword().equals(pdb.getPassword())) {
+	    	JSONObject obj = new JSONObject();
+	    	obj.put("login", "success");
+	    	obj.put("last_login", pdb.getLast_login());
+	    	obj.put("image_url", pdb.getImage_url());
+	    	return Response.status(Status.OK).entity(obj.toJSONString()).build();
+	    }
+	    JSONObject obj = new JSONObject();
+    	obj.put("login", "failed");
+	    return Response.status(Status.OK).entity(obj.toJSONString()).build();
 	}
-
+	
 	@PUT
 	@Path("customer")
-	public void updateperson(person a1) {
-		if (repo.getperson(a1.getEmail()).getEmail() == null) {
+	public person updateperson(person a1)
+	{
+		System.out.println(a1);
+		
+		if(repo.getperson(a1.getEmail()).getEmail()== null)
+		{
 			repo.create(a1);
-		} else {
+		}
+		else
+		{
 			repo.update(a1);
 		}
+		return a1;
 	}
-
+	
 	@DELETE
 	@Path("customer/{email}")
-	public void killperson(@QueryParam("email") String email) {
-		if(email == null) {
-			return;
-		}
-		repo.delete(email);
+	public person killperson(@QueryParam("email") String email)
+	{
+		person a=repo.getperson(email);
+		
+		if(a.getEmail()!=null)
+			repo.delete(email);
+		
+		return a;
 	}
-
+	
 }
