5cb8bf1c938fec6de39f80c1012f33c5e7606b90
rojraman <rojraman@iu.edu>
2019-02-27 20:19:43 -0500

    Implemented Issue #107

--- server/__init__.py
+++ server/__init__.py
@@ -1,5 +1,11 @@
 from app import App
 from waitress import serve
+import sys
 
-serve(App().get_api(), host='0.0.0.0', port=7000) 
+if (len(sys.argv) == 3 and sys.argv[1] == "PORT"):
+    PORT = sys.argv[2]
+else:
+    PORT = 7000
+
+serve(App().get_api(), host='0.0.0.0', port=PORT) 
 
