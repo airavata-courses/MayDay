bb283dec85403317bc2121ce6e9bb6264964b243
Arpit Bansal <arpitbans8@gmail.com>
2019-02-27 23:55:32 -0500

    Fixed CORS filter in web.xml

--- com.microservice.red/src/main/java/com/microservice/filter/CorsFilter.java
+++ com.microservice.red/src/main/java/com/microservice/filter/CorsFilter.java
@@ -13,7 +13,6 @@ public class CorsFilter implements ContainerResponseFilter {
 	@Override
 	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
 			throws IOException {
-		System.out.println("filtered");
 		responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
 		responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
 		responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
