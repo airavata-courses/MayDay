3e781124b58fe0eb7d38a1fdae507e14524ac746
rojraman <rojraman@iu.edu>
2019-03-01 16:58:55 -0500

    Issue #98 implemented

--- server/__init__.py
+++ server/__init__.py
@@ -1,6 +1,9 @@
 from app import App
 from waitress import serve
 import config as config
+from zookeeper import ZookeeperHandler
 
+ZookeeperHandler.registerAuthService(App().get_zk(),config.PORT)
 serve(App().get_api(), host='0.0.0.0', port=config.PORT) 
+
 
--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@ import falcon
 
 from search import Search
 from test import Test
+from zookeeper import ZookeeperHandler
 from falcon_cors import CORS
 cors = CORS(allow_origins_list=['http://localhost:3000', 'http://js-168-167.jetstream-cloud.org:4200'],
             allow_all_headers=True,
@@ -17,13 +18,18 @@ class App:
 		self.api  = falcon.API(middleware=[cors.middleware])
 		self.search = Search()
 		self.test = Test()
+		self.zk = ZookeeperHandler()
 		self.add_api_routes()
 
 	def add_api_routes(self):
 		self.api.add_route('/search/recent',self.search)
+		self.api.add_route('/zookeeper',self.search)
 		self.api.add_route('/test',self.test)
 	
 	def get_api(self):
 		return self.api
+	
+	def get_zk(self):
+		return self.zk
 
 
