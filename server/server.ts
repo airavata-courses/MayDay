fb4a388c9514c2f05ce39af3df3a5ace3ebe83ba
KalaRajam <shekhar957@gmail.com>
2019-03-02 12:30:20 -0500

    added zookeeper client for purple #97

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,42 @@
 import app from './app';
-//mport ZookeeperClient from '../zookeeper_client/zk_client';
+import ZookeeperClient from '../zookeeper_client/zk_client';
+import { OptionList, Content } from 'command-line-usage';
+import commandLineUsage = require('command-line-usage');
+import { OptionDefinition } from 'command-line-args';
+import commandLineArgs = require('command-line-args');
 
-const PORT: number = Number(process.argv[2]) || 3000;
-//new ZookeeperClient(['localhost:2181'], '/testpath2').connectToEnsemble();
+const cmdOptions: OptionDefinition[] = [
+    { name: 'port', type: Number, multiple: false, defaultValue: 3000 },
+    { name: 'zkservers', type: String, multiple: true },
+    { name: 'help', type: String }
+];
+
+const sections: (OptionList | Content)[] = [
+    {
+      header: 'Purple Microservice',
+      content: 'Gateway to external APIs'
+    },
+    {
+      header: 'Options',
+      optionList : cmdOptions
+    }
+  ] 
+const usage = commandLineUsage(sections)
+
+const options = commandLineArgs(cmdOptions, {partial: true});
+if (options['--help']) {
+    console.log(usage);
+    process.exit(0);
+}
+
+const PORT: number = options['port'];
+if(options['zkservers']) {
+    const ZKSERVERS: string[] = options['zkservers'];
+    const zk = new ZookeeperClient(ZKSERVERS, '/purpleznode1');
+    zk.connectToEnsemble();   
+}
+
+
 app.listen(PORT,() => {
     console.log('Purple listening on port ' + PORT);
 });
---
+++ zookeeper_client/zk_client.ts
@@ -0,0 +1,41 @@
+
+import {join as loJoin} from 'lodash';
+import { createClient, CreateMode, Option, Client } from 'node-zookeeper-client';
+
+export default class ZookeeperClient {
+
+    zk_client: Client;
+    zNodePath: string;
+    zkConnectionString: string[];
+    clientOptions: Option;
+    constructor(zkConnectionString: string[], zNodePath: string) {
+        this.zkConnectionString = zkConnectionString;
+        this.zNodePath = zNodePath;
+        this.clientOptions = {
+            retries: 1,
+            sessionTimeout: 2000,
+            spinDelay: 500
+        }
+        this.zk_client = createClient(loJoin(zkConnectionString, ','), this.clientOptions);
+    }
+
+    createNode() {
+        this.zk_client.create(this.zNodePath, CreateMode.EPHEMERAL, (error: Error) => {
+            if(error) {
+                console.log("can't create path: ",error);
+            } else {
+                console.log('connected to znode ', this.zNodePath);
+            }
+            this.zk_client.close();
+        });
+    }
+
+    connectToEnsemble() {
+        this.zk_client.once('connected', () => {
+            console.log('Connected to the zookeper server.');
+            this.createNode();
+        });
+
+        this.zk_client.connect();
+    }
+}
