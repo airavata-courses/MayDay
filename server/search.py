76e46486d0f3cf078cfa868214bdf5873f9bba8b
rojraman <rojraman@iu.edu>
2019-03-03 14:13:22 -0500

    Added try and exception handling

--- server/search.py
+++ server/search.py
@@ -10,36 +10,36 @@ from db_connect import DB
 class Search(object):
 
     def on_post(self, req, res):
-       # try:
-        posted_data = json.loads(req.stream.read())
-        if "recent_result" not in posted_data:
-            errmsg = {
-                "msg" : "Invalid request format"
-            }
-            res.set_header('response', '400')
-            res.set_header('Access-Control-Allow-Origin', '*')
-            res.set_header('Access-Control-Allow-Methods', '*')
-            res.set_header('Access-Control-Allow-Headers', '*')
-            res.set_header('Access-Control-Max-Age', 1728000)  #20 days
-            res.status = falcon.HTTP_400
-            res.body = json.dumps(errmsg, ensure_ascii=False,sort_keys=True, indent=4)
-        else:
-            #ob = db()
-            DB().get_db().executeSQL(DB().get_db().readSQLFromFile('sanjeevni_db.sql'))
-            obj ={
-                "code" : 201,
-                "message" : "Created",
-                "data":posted_data
-            }
-            DB().get_db().executeSQL("INSERT INTO sanjeevi_search_history VALUES ('"+obj["data"]["recent_result"][0]["userid"]+"', '"+obj["data"]["recent_result"][0]["search_string"]+"', '"+str(obj["data"]["recent_result"][0]["endpoint"])+"' ,CURRENT_TIMESTAMP);")
-            DB().get_db().executeSQL("SELECT * from sanjeevi_search_history")
-            #print ob.getResponse()
-            DB().get_db().commit()
-            DB().get_db().destroy()
-            res.set_header('response', '201 Created')
-            res.set_header('Access-Control-Allow-Origin', '*')
-            res.body = json.dumps(OrderedDict(obj), ensure_ascii=False)
-            res.status = falcon.HTTP_201
-        # except Exception as e:
-        #     res.status = falcon.HTTP_500
-        #     res.body = json.dumps({'status': 0, 'message': 'Server Error'})
+        try:
+            posted_data = json.loads(req.stream.read())
+            if "recent_result" not in posted_data:
+                errmsg = {
+                    "msg" : "Invalid request format"
+                }
+                res.set_header('response', '400')
+                res.set_header('Access-Control-Allow-Origin', '*')
+                res.set_header('Access-Control-Allow-Methods', '*')
+                res.set_header('Access-Control-Allow-Headers', '*')
+                res.set_header('Access-Control-Max-Age', 1728000)  #20 days
+                res.status = falcon.HTTP_400
+                res.body = json.dumps(errmsg, ensure_ascii=False,sort_keys=True, indent=4)
+            else:
+                #ob = db()
+                DB().get_db().executeSQL(DB().get_db().readSQLFromFile('sanjeevni_db.sql'))
+                obj ={
+                    "code" : 201,
+                    "message" : "Created",
+                    "data":posted_data
+                }
+                DB().get_db().executeSQL("INSERT INTO sanjeevi_search_history VALUES ('"+obj["data"]["recent_result"][0]["userid"]+"', '"+obj["data"]["recent_result"][0]["search_string"]+"', '"+str(obj["data"]["recent_result"][0]["endpoint"])+"' ,CURRENT_TIMESTAMP);")
+                DB().get_db().executeSQL("SELECT * from sanjeevi_search_history")
+                #print ob.getResponse()
+                DB().get_db().commit()
+                DB().get_db().destroy()
+                res.set_header('response', '201 Created')
+                res.set_header('Access-Control-Allow-Origin', '*')
+                res.body = json.dumps(OrderedDict(obj), ensure_ascii=False)
+                res.status = falcon.HTTP_201
+        except Exception as e:
+            res.status = falcon.HTTP_500
+            res.body = json.dumps({'status': 0, 'message': 'Server Error'})
