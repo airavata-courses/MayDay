0c486c84386291812921b108e6ee3b4395300365
rojraman <rojraman@iu.edu>
2019-03-03 01:43:06 -0500

    Null checks for data in practices popup

--- src/app/components/practices-profile/practices-profile.component.ts
+++ src/app/components/practices-profile/practices-profile.component.ts
@@ -21,31 +21,33 @@ export class PracticesProfileComponent implements OnInit {
 
   ngOnInit() {
     console.log(this.data);
-    this.pratice_center = this.data['practices'][0]['name'];
+    if (this.data['practices'][0] != null && this.data['practices'][0] !== undefined) {
+      this.pratice_center = this.data['practices'][0]['name'];
 
-    if(this.data['practices'][0]['visit_address'] !== null && this.data['practices'][0]['visit_address'] !== undefined ){
-      this.address = 'Address: '+this.data['practices'][0]['visit_address']['street'] + ',\n'+this.data['practices'][0]['visit_address']['street2']+ ',\n'+this.data['practices'][0]['visit_address']['city']+ ',\n'+this.data['practices'][0]['visit_address']['state']+ ',\n'+this.data['practices'][0]['visit_address']['zip'];
-    }
-    if(this.data['practices'][0]['phones'] !== null && this.data['practices'][0]['phones'] !== undefined ){
-      this.phone = 'Contact: '+this.data['practices'][0]['phones'][0]['number'];
-    }
+      if (this.data['practices'][0]['visit_address'] !== null && this.data['practices'][0]['visit_address'] !== undefined) {
+        this.address = 'Address: ' + this.data['practices'][0]['visit_address']['street'] + ',\n' + this.data['practices'][0]['visit_address']['street2'] + ',\n' + this.data['practices'][0]['visit_address']['city'] + ',\n' + this.data['practices'][0]['visit_address']['state'] + ',\n' + this.data['practices'][0]['visit_address']['zip'];
+      }
+      if (this.data['practices'][0]['phones'] !== null && this.data['practices'][0]['phones'] !== undefined) {
+        this.phone = 'Contact: ' + this.data['practices'][0]['phones'][0]['number'];
+      }
 
-    this.haswebsite = false;
-    if(this.data['practices'][0]['website'] !== null && this.data['practices'][0]['website'] !== undefined){
-      this.website = this.data['practices'][0]['website'];
-      this.haswebsite = true;
-      console.log(this.website + 'website');
-    }else{
-      this.website = 'N/A';
       this.haswebsite = false;
-    }
-    
-    if(this.data['practices'][0]['email'] !== null && this.data['practices'][0]['email'] !== undefined){
-      this.email = this.data['practices'][0]['email']; 
-      this.hasemail = true;
-    }else{
-      this.email = 'N/A';
-      this.hasemail = false;
+      if (this.data['practices'][0]['website'] !== null && this.data['practices'][0]['website'] !== undefined) {
+        this.website = this.data['practices'][0]['website'];
+        this.haswebsite = true;
+        console.log(this.website + 'website');
+      } else {
+        this.website = 'N/A';
+        this.haswebsite = false;
+      }
+
+      if (this.data['practices'][0]['email'] !== null && this.data['practices'][0]['email'] !== undefined) {
+        this.email = this.data['practices'][0]['email'];
+        this.hasemail = true;
+      } else {
+        this.email = 'N/A';
+        this.hasemail = false;
+      }
     }
   }
 
