85903b4ba466533616ed2d37d22b5a833706af7b
Arpit Bansal <arpitbans8@gmail.com>
2019-03-02 21:38:11 -0500

    Profile and practices Pop-up #102 #103

--- src/app/components/doctor-bio/doctor-bio.component.ts
+++ src/app/components/doctor-bio/doctor-bio.component.ts
@@ -7,11 +7,25 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
   styleUrls: ['./doctor-bio.component.css']
 })
 export class DoctorBioComponent implements OnInit {
-
+  specialties: any;
+  ratings:any;
+  ratings_stars: any;
+  israted: boolean;
+   
   constructor(public dialogRef: MatDialogRef<DoctorBioComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit() {
     console.log(this.data);
+    this.specialties = this.data['specialties'][0];
+    this.ratings = '';
+    this.israted = false;
+    if(this.data['ratings'][0] !== null && this.data['ratings'][0] !== undefined ){
+      this.ratings_stars = this.data['ratings'][0]['image_url_small'];
+      this.ratings =  'Ratings : '+ this.data['ratings'][0]['rating'];
+      this.israted = true;
+    }
+    
+ 
   }
 }
--- src/app/components/practices-profile/practices-profile.component.ts
+++ src/app/components/practices-profile/practices-profile.component.ts
@@ -9,12 +9,44 @@ import { DoctorProfileComponent } from '../doctor-profile/doctor-profile.compone
 })
 export class PracticesProfileComponent implements OnInit {
 
+  pratice_center: any;
+  address: any;
+  phone: any;
+  website: any;
+  haswebsite: boolean;
+  email: any;
+  hasemail: boolean;
   constructor(public dialogRef: MatDialogRef<DoctorProfileComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit() {
-    // roja you will get the data here
     console.log(this.data);
+    this.pratice_center = this.data['practices'][0]['name'];
+
+    if(this.data['practices'][0]['visit_address'] !== null && this.data['practices'][0]['visit_address'] !== undefined ){
+      this.address = 'Address: '+this.data['practices'][0]['visit_address']['street'] + ',\n'+this.data['practices'][0]['visit_address']['street2']+ ',\n'+this.data['practices'][0]['visit_address']['city']+ ',\n'+this.data['practices'][0]['visit_address']['state']+ ',\n'+this.data['practices'][0]['visit_address']['zip'];
+    }
+    if(this.data['practices'][0]['phones'] !== null && this.data['practices'][0]['phones'] !== undefined ){
+      this.phone = 'Contact: '+this.data['practices'][0]['phones'][0]['number'];
+    }
+
+    this.haswebsite = false;
+    if(this.data['practices'][0]['website'] !== null && this.data['practices'][0]['website'] !== undefined){
+      this.website = this.data['practices'][0]['website'];
+      this.haswebsite = true;
+      console.log(this.website + 'website');
+    }else{
+      this.website = 'N/A';
+      this.haswebsite = false;
+    }
+    
+    if(this.data['practices'][0]['email'] !== null && this.data['practices'][0]['email'] !== undefined){
+      this.email = this.data['practices'][0]['email']; 
+      this.hasemail = true;
+    }else{
+      this.email = 'N/A';
+      this.hasemail = false;
+    }
   }
 
 }
