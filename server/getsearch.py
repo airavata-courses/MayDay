1ad62641ce7d2c12369d0bf703db51567d726b01
rojraman <rojraman@iu.edu>
2019-03-03 02:00:34 -0500

    modified db structure and post req

--- server/getsearch.py
+++ server/getsearch.py
@@ -22,8 +22,8 @@ class GetSearch(object):
                 record["userid"] = userid
             record["search_string"] = result[count]
             count += 1
-            record["req_param"] = result[count]
-            count += 1
+            # record["req_param"] = result[count]
+            # count += 1
             record["endpoint"] = result[count]
             count += 1
             record["frequency"] = result[count]
@@ -38,10 +38,10 @@ class GetSearch(object):
             ob.executeSQL(ob.readSQLFromFile('sanjeevni_db.sql'))
             if "userid" not in req.params:
                 userid = ""
-                ob.executeSQL("SELECT h.search_string , h.req_param, h.endpoint, count(h.search_string) from sanjeevi_search_history as h GROUP BY h.search_string")
+                ob.executeSQL("SELECT h.search_string , h.endpoint, count(h.search_string) from sanjeevi_search_history as h GROUP BY h.search_string")
             else:
                 userid = req.params['userid']
-                ob.executeSQL("SELECT TOP 10 h.search_string , h.req_param, h.endpoint, count(h.search_string) from sanjeevi_search_history as h where h.userid like '"+req.params["userid"]+"' GROUP BY h.search_string")
+                ob.executeSQL("SELECT TOP 10 h.search_string , h.endpoint, count(h.search_string) from sanjeevi_search_history as h where h.userid like '"+req.params["userid"]+"' GROUP BY h.search_string")
             data = self.parseJson_fromQuery(ob.getResponse() ,userid)
             ob.destroy()
             resp.set_header('response', '200 OK')
--- server/sanjeevni_db.sql
+++ server/sanjeevni_db.sql
@@ -3,14 +3,12 @@
 CREATE TABLE IF NOT EXISTS sanjeevi_search_history(
 	userid VARCHAR,
 	search_string VARCHAR,
-	req_param VARCHAR,
 	endpoint VARCHAR,
 	time_stamp timestamp
 );
 
 CREATE TABLE IF NOT EXISTS sanjeevi_search_history_temp(
 	search_string VARCHAR,
-	req_param VARCHAR,
 	endpoint VARCHAR
 );
 -- INSERT INTO sanjeevi_search_history VALUES ('roja.raman@iu.edu', 'Mark', '{"limit": "10","location": "37.773,-122.413,100","name": "mark","skip": "0", "user_location": "37.773,-122.413"}','/alldoctors' ,CURRENT_TIMESTAMP);
