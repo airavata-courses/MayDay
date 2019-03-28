3ea3d1c1c871954b6847bb42f4ce47d6d9add7bb
shashank <42894144+KalaRaja@users.noreply.github.com>
2019-03-28 13:50:19 -0400

    reverted for grading, we had missed this.

--- src/environments/environment.cloud.ts
+++ src/environments/environment.cloud.ts
@@ -1,9 +1,21 @@
-export const environment = {
+/*export const environment = {
     production: false,
     endpoints: {
       'purple': 'http://149.165.156.182:30009',
       'blue': 'http://149.165.156.182:30008',
       'red': 'http://149.165.156.182:30007/rest/customers',
+      'ipinfo': 'https://ipinfo.io/?token=3f660cf19bbe14',
+      'geocode': 'https://www.mapquestapi.com/geocoding/v1/reverse?key=EvAFDNlMGI6PeGpkR33PAUfF61AvIliz'
+    }
+  };*/
+
+//revert for grading
+export const environment = {
+    production: false,
+    endpoints: {
+      'purple': 'http://149.165.169.31:3000',
+      'blue': 'http://149.165.168.77:7000',
+      'red': 'http://149.165.169.159:8080/rest/customers',
       'ipinfo': 'https://ipinfo.io/?token=3f660cf19bbe14',
       'geocode': 'https://www.mapquestapi.com/geocoding/v1/reverse?key=EvAFDNlMGI6PeGpkR33PAUfF61AvIliz'
     }
