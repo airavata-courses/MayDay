432a356978b76cca476df603265864839013abae
rojraman <rojraman@iu.edu>
2019-03-28 02:27:35 -0400

    removed zookeeper

--- server/__init__.py
+++ server/__init__.py
@@ -3,7 +3,7 @@ from waitress import serve
 import config as config
 from zookeeper import ZookeeperHandler
 
-ZookeeperHandler.registerAuthService(App().get_zk(),config.PORT)
+#ZookeeperHandler.registerAuthService(App().get_zk(),config.PORT)
 serve(App().get_api(), host='0.0.0.0', port=config.PORT) 
 
 
--- server/config.py
+++ server/config.py
@@ -2,12 +2,14 @@ import sys
 
 DEFAULT = 7000
 ZK_DEFAULT = '149.165.170.230:2181'
-if (len(sys.argv) >= 3 or sys.argv[1] == "PORT"):
-    PORT = sys.argv[2]
-else:
+if (len(sys.argv) == 1):
     PORT = DEFAULT
+elif (len(sys.argv) >= 3 or sys.argv[1] == "PORT"):
+    PORT = sys.argv[2]
+# else:
+#     PORT = DEFAULT
 
-if(len(sys.argv) >= 3 or sys.argv[2] == "zkserver"):
-    ZK = sys.argv[4]
-else:
-    ZK = ZK_DEFAULT
+# if(len(sys.argv) >= 3 or sys.argv[2] == "zkserver"):
+#     ZK = sys.argv[4]
+# else:
+#     ZK = ZK_DEFAULT
