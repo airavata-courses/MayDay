862cd219946530b5379d0527774d4d2ad15c6479
rojraman <rojraman@iu.edu>
2019-04-03 16:01:39 -0400

    Removed H2 import

--- server/app.py
+++ server/app.py
@@ -4,7 +4,6 @@ from test import Test
 from zookeeper import ZookeeperHandler
 from falcon_cors import CORS
 from getsearch import GetSearch
-from connect import H2Connection as db
 cors = CORS(allow_origins_list=['http://localhost:3000', 'http://js-168-167.jetstream-cloud.org:4200', 'http://js-156-182.jetstream-cloud.org:30006'],
             allow_all_headers=True,
             allow_all_methods=True,
