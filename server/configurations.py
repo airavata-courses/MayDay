867cc63912e18670fb590ed1dec115e487ba6c40
rojraman <rojraman@iu.edu>
2019-04-02 20:31:16 -0400

    configparser issue

--- server/configurations.py
+++ server/configurations.py
@@ -1,10 +1,10 @@
 import os
-import ConfigParser
+import configparser
 
 class Configurations:
 
     def __init__(self):
-        self.config = ConfigParser.ConfigParser()
+        self.config = configparser.ConfigParser()
         self.config.read(
             os.path.join(os.path.dirname(__file__),'database.ini')
         )
