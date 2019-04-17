ee0484e221e9949c406466bc12e7bd6efc49ae0a
shashank <42894144+KalaRaja@users.noreply.github.com>
2019-04-17 17:38:43 -0400

    changed the endpoints to ha-proxy endpoints

--- src/environments/environment.cloud.ts
+++ src/environments/environment.cloud.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: false,
     endpoints: {
-      'purple': 'http://149.165.156.182:30009',
-      'blue': 'http://149.165.156.182:30008',
-      'red': 'http://149.165.156.182:30007/rest/customers',
+      'purple': 'http://149.165.170.250:8081',
+      'blue': 'http://149.165.170.250:8082',
+      'red': 'http://149.165.170.250:8083/rest/customers',
       'ipinfo': 'https://ipinfo.io/?token=3f660cf19bbe14',
       'geocode': 'https://www.mapquestapi.com/geocoding/v1/reverse?key=EvAFDNlMGI6PeGpkR33PAUfF61AvIliz'
     }
